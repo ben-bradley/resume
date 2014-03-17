cea5ed87602c7545f4b849fcf38ac002d886430f
Ben Bradley <ben@bradleyit.com>
2014-03-17 11:02:25 -0700

    added json view, converted resume file to json

--- resume.js
+++ resume.js
@@ -10,27 +10,75 @@ var config = {
 	port: 8080
 };
 
+// gravatar uses a md5 hashed email address for avatar reference
 var md5sum = crypto.createHash('md5');
 
 // global for the list of resumes
-var resumes = [];
+var resumes = {
+	
+	list: [],
+	
+	get: function(id) {
+		var got = undefined;
+		this.list.forEach(function(resume) {
+			if (resume.name == id)
+				got = resume;
+		});
+		return got;
+	},
+	
+	add: function(data, res) {
+		
+	},
+	
+	update: function(data, res) {
+		
+	},
+	
+	remove: function(data, res) {
+		
+	}
+	
+}
 
 app.configure(function() {
 	
-	// return the basic ui
+	// return the static elements of the ui
 	app.use('/', express.static(__dirname+'/ui'));
 	
 	// return an array of resumes
 	app.get('/api/resumes', function(req, res) {
-		res.send(resumes);
+		res.send(resumes.list);
 	});
 	
 	// return a specific resume
 	app.get('/api/resumes/:person', function(req, res) {
-		resumes.forEach(function(resume) {
-			if (resume.name == req.params.person)
-				res.send(resume);
-		});
+		res.send(resumes.get(req.params.person));
+	});
+	
+	// TODO: build the 'save new' route
+	app.post('/api/resumes/:person', function(req, res) {
+		// validate that the person is new
+		var resume = resumes.get(req.params.person);
+		if (!resume)
+			resumes.add(req.body, res);
+		else
+			resumes.update(req.body, res);
+	});
+	
+	// TODO: build the 'update' route
+	app.put('/api/resumes/:person', function(req, res) {
+		// validate that the person is NOT new
+		var resume = resumes.get(req.params.person);
+		if (!resume)
+			resumes.add(req.body, res);
+		else
+			resumes.update(req.body, res);
+	});
+	
+	// TODO: build the 'delete' route
+	app.delete('/api/resumes/:person', function(req, res) {
+		resumes.remove(req.params.person, res);
 	});
 	
 });
@@ -39,10 +87,10 @@ app.configure(function() {
 fs.readdir(__dirname+'/resumes', function(err, files) {
 	files.forEach(function(file) {
 		try {
-			var resume = require(__dirname+'/resumes/'+file);
+			resume = JSON.parse(fs.readFileSync(__dirname+'/resumes/'+file));
 			resume._file = __dirname+'/resumes/'+file;
 			resume._gravatarUrl = 'http://www.gravatar.com/avatar/'+md5sum.update(resume.email).digest('hex');
-			resumes.push(resume);
+			resumes.list.push(resume);
 		}
 		catch(err) {
 			console.error('Can\'t import file: '+__dirname+file);
--- ui/js/views/resume.js
+++ ui/js/views/resume.js
@@ -3,20 +3,23 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
-	'text!/templates/resume.html'
-], function($, _, Backbone, resumeHtml){
+	'text!/templates/resume.html',
+	'text!/templates/resume_json.html'
+], function($, _, Backbone, resumeHtml, resumeJsonHtml){
 	
 	var ResumeView = Backbone.View.extend({
 		
 		el: $('#resume'),
 		
-		initialize: function() {
+		initialize: function(options) {
+			this.showJson = options.showJson;
 			this.render();
 		},
 		
 		render: function() {
 			var data = { resume: this.model.toJSON() };
-			var compiledTemplate = _.template(resumeHtml, data);
+			var template = (this.showJson) ? resumeJsonHtml : resumeHtml;
+			var compiledTemplate = _.template(template, data);
 			this.$el.empty();
 			this.$el.html(compiledTemplate);
 		}
